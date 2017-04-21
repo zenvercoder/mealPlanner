79b2c4d6fbb34c6e8ef61985764b37ad9e6aac16
Lisa Hull <lisa@jchull.com>
2017-04-21 13:48:19 -0600

    grocery list :sparkles:

--- public/scripts/specials.js
+++ public/scripts/specials.js
@@ -5,8 +5,16 @@ var displayHidden = function () {
 $('.hide-field').hide();
 
 
-$( ".showSearch" ).click(function() {
-    $( ".search-field" ).toggle("slow");
+$(".showSearch").click(function () {
+    $(".search-field").toggle("slow");
+});
+
+$(".grocery-list-add").click(function () {
+
+    var groceryItem = $(this).attr("data-grocery-item").split(",")[0];
+    var checkbox = $('<input type="checkbox">"');
+    $(".recipe-list ul .grocery-list-item").append("<li>").append(groceryItem);
+    // console.log($(this).attr("data-grocery-item").split(",")[0]);
 });
 
 $(document).ready(displayHidden);
